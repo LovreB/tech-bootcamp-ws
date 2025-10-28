5208ad11be97e4f10224ff985fcf4d82e7648db0
Emma Segolsson <emma.segolsson@outlook.com>
2025-10-28 11:12:31 +0100

    fix small errors

--- app/data/getMovies.ts
+++ app/data/getMovies.ts
@@ -1,9 +1,9 @@
-import { Movie } from "../types/Movie";
+import { MovieDto } from "../types/MovieDto";
 import { fetchEndpoint } from "./fetchEndpoint";
 
 /* This function calls our BFF. The available endpoins are found in the documentation */
-export const getMovies = async (title: string): Promise<Movie[]> => {
+export const getMovies = async (title: string) => {
   const url = `/api/movies?title=${title}&userId=${process.env.NEXT_PUBLIC_USERNAME}`;
 
-  return fetchEndpoint(url);
+  return fetchEndpoint<MovieDto[]>(url);
 };
--- app/data/removeFavorite.ts
+++ app/data/removeFavorite.ts
@@ -1,9 +1,9 @@
-import { Movie } from "../types/Movie";
+import { MovieDto } from "../types/MovieDto";
 import { fetchEndpoint } from "./fetchEndpoint";
 
 /* This function calls our BFF, the endpoint that returns the user's favorites.
  * The available endpoins are found in the documentation */
-export const removeFavorite = async (movieId: string): Promise<Movie[]> => {
+export const removeFavorite = async (movieId: string): Promise<MovieDto[]> => {
   const url = `/api/users/${process.env.NEXT_PUBLIC_USERNAME}/favorites/${movieId}`;
   return fetchEndpoint(url, { method: "DELETE" });
 };
